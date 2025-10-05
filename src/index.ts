8219088e5f494b6c2a897590ef0f54759a670b4a
Cleboost <clement.balarot@gmail.com>
2025-10-05 20:32:07 +0200

    Add global font and export modal components
    Imported a global Google font stylesheet and included it in the project. Also added Modal and About components to the exports in index.ts for broader usage.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import "./css/global.css";
+
 import Switch from "./components/switch.vue";
 import Button from "./components/button.vue";
 import RowButton from "./components/row/rowButton.vue";
 import RowLink from "./components/row/rowLink.vue";
 import BoxRow from "./components/row/boxRow.vue";
+import Modal from "./components/modals/modal.vue";
+import About from "./components/modals/about.vue";
 
-export { Switch, Button, RowButton, RowLink, BoxRow };
+export { Switch, Button, RowButton, RowLink, BoxRow, Modal, About };
